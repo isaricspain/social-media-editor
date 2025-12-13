68bd8f8f7fe62789423de5967a4bb54b546fb001
Ubuntu <ubuntu@ip-172-31-64-121.ec2.internal>
2025-12-13 14:13:49 +0000

    Release: 2025-12-13T14-13-49

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (username, email, password) VALUES
+('testuser', 'test@example.com', '$2a$10$X0cpO3mUrRSSGOZBq/bZd.xEwGnI8EQr8sxaPJiCr8dIDu9w7PO5e');
+
+-- Password is 'password123' (BCrypt encoded)
